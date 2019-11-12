f4130aebf3e385fcf5f0625a883872862bea2a0e
Rudy <d.link_52@hotmail.com>
2019-11-11 22:36:21 -0600

    Fix settings

--- events/settings.py
+++ events/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '88mu+&zw*$q#xkha*47uyom9!!9k)uvjit8i&jw-+#e$umwg9j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
